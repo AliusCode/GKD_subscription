7d68df51458974f645ce30bcccc88baf9a4536ba
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-02-24 23:30:52 +0800

    fix: 下厨房 局部广告-卡片广告

--- src/apps/com.xiachufang.ts
+++ src/apps/com.xiachufang.ts
@@ -36,45 +36,40 @@ export default defineGkdApp({
     {
       key: 1,
       name: '局部广告-卡片广告',
+      fastQuery: true,
+      activityIds: '.activity.recipe.RecipeDetailActivity',
       rules: [
         {
           key: 1,
-          fastQuery: true,
-          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
           matches: '[id="com.xiachufang:id/ad_close"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/13348710',
         },
         {
           key: 2,
-          fastQuery: true,
-          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
           matches:
             '[id="com.xiachufang:id/jad_feed_close"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/13363079',
         },
         {
           key: 3,
-          fastQuery: true,
-          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
           matches: '@[clickable=true] > [text="不感兴趣"]',
           snapshotUrls: 'https://i.gkd.li/i/13454534',
         },
         {
           key: 4,
-          fastQuery: true,
-          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
           matches:
             '@ImageView[childCount=0][visibleToUser=true] < FrameLayout[childCount=1] - LinearLayout[childCount=2] > [text="下载应用" || text="立即下载" || text="查看详情" || text="领取优惠" || text="进入小程序" || text="了解更多"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/13484117',
         },
         {
           key: 5,
-          fastQuery: true,
-          activityIds: '.activity.recipe.RecipeDetailActivity',
           matches:
-            '@ImageView[childCount=0][visibleToUser=true] < FrameLayout[childCount=1] <3 FrameLayout +2 FrameLayout[childCount=2] > LinearLayout[childCount=2] > [text^="立即"]',
+            '@ImageView[id=null][text=null][width<63][height<63] < FrameLayout <3 FrameLayout <<4 [vid="fl_container"]',
           exampleUrls: 'https://e.gkd.li/f9b63d84-3363-4330-93b9-156d6f3d3d32',
-          snapshotUrls: 'https://i.gkd.li/i/18249193',
+          snapshotUrls: [
+            'https://i.gkd.li/i/18249193',
+            'https://i.gkd.li/i/25508978',
+          ],
         },
       ],
     },
@@ -84,10 +79,9 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
-          key: 1,
           fastQuery: true,
-          activityIds: 'com.xiachufang.activity.recipe.RecipeDetailActivity',
-          matches: '@[desc="关闭"] - [text^="厨房问卷"]',
+          activityIds: '.activity.recipe.RecipeDetailActivity',
+          matches: '@[clickable=true][desc="关闭"] - [text^="厨房问卷"]',
           snapshotUrls: 'https://i.gkd.li/i/13363042',
         },
       ],
