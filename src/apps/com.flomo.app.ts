125b7dca78f8416417c4c1be0f45eb3725951bd0
天方夜谭 <tianfangyetan01@qq.com>
2026-03-27 22:56:53 +0800

    feat: flomo浮墨笔记-启动后自动点击新建卡片

--- src/apps/com.flomo.app.ts
+++ src/apps/com.flomo.app.ts
@@ -19,5 +19,21 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-启动后自动点击新建卡片',
+      desc: '仅开屏后触发一次',
+      rules: [
+        {
+          matchTime: 5000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          fastQuery: true,
+          activityIds: '.ui.activity.MainActivity',
+          matches: '[vid="btnCreateMemo"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/26307856',
+        },
+      ],
+    },
   ],
 });
