65723462465c55054ec668c03b3331c2584247cd
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-04-09 00:33:46 +0800

    feat: 百度网盘 更新提示
    - 新加activityId

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -52,11 +52,16 @@ export default defineGkdApp({
       resetMatch: 'app',
       rules: [
         {
-          activityIds: ['.ui.MainActivity', '.ui.cloudp2p.RichMediaActivity'],
+          activityIds: [
+            '.ui.MainActivity',
+            '.ui.cloudp2p.RichMediaActivity',
+            '.module.sharelink.ChainVerifyActivity',
+          ],
           matches: '[text="立即更新"] -2 [text="下次再说"]', //使用ID会导致误触（例如删除确认https://i.gkd.li/i/13069049）
           snapshotUrls: [
             'https://i.gkd.li/i/16118181',
             'https://i.gkd.li/i/24834209',
+            'https://i.gkd.li/i/26574534',
           ],
         },
       ],
