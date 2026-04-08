be596e4b9a3b599be9773024ead671c2939427cd
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-04-08 22:23:33 +0800

    perf: LocalSend 功能类-文件传输时自动点击[高级]
    - 之前对forcedTime参数理解不够,现换成100秒
    - 用了 << 在GKD会归类为缓慢查询
    - <<4 和 <<n 的区别还没研究透, https://github.com/orgs/gkd-kit/discussions/299

--- src/apps/org.localsend.localsend_app.ts
+++ src/apps/org.localsend.localsend_app.ts
@@ -7,17 +7,16 @@ export default defineGkdApp({
     {
       key: 1,
       name: '功能类-文件传输时自动点击[高级]',
-      desc: '仅触发1~2次,切换app后重置', // https://github.com/Lin-arm/GKD_subscription/discussions/93#discussioncomment-16401537
       rules: [
         {
           fastQuery: true,
           matchRoot: true,
           actionCd: 3500,
           // actionMaximum: 1,
-          forcedTime: 15000,
+          forcedTime: 100000, // 100秒内一直主动参与屏幕查询  https://github.com/Lin-arm/GKD_subscription/discussions/93
           activityIds: '.MainActivity',
           matches:
-            '@[desc="高级"] < View <2 View[childCount=2] <<4 [id="android:id/content"]',
+            '@[desc="高级"] < View <2 View[childCount=2] < View < View < FrameLayout < [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/26321028',
           excludeSnapshotUrls: 'https://i.gkd.li/i/26314724', // 高级信息已显示
           exampleUrls: 'https://e.gkd.li/54a7208c-03b2-4e74-a8ed-99829d72c8c5',
