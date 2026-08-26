ecfcfa3a797059dafbecadc0b1c42d7e3497430c
天方夜谭 <tianfangyetan01@qq.com>
2026-08-26 14:07:26 +0800

    feat: Kimi 评价提示

---
+++ src/apps/com.moonshot.kimichat.ts
@@ -0,0 +1,20 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.moonshot.kimichat',
+  name: 'Kimi',
+  groups: [
+    {
+      key: 1,
+      name: '评价提示',
+      rules: [
+        {
+          activityIds: '.MainActivity',
+          matches:
+            '[text="残忍拒绝"] < @View[clickable=true] -n [text^="感谢喜欢 Kimi"][text*="打个分吧"]',
+          snapshotUrls: 'https://i.gkd.li/i/31449987',
+        },
+      ],
+    },
+  ],
+});
